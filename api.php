c304f0030c27a0bfee2939d074c364b56d0dd519
Dat Nguyen <dat@duo-studio.co>
2023-09-07 00:47:45 -0400

    anchor

---
+++ api.php
@@ -0,0 +1,31 @@
+<?php 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+if($_POST){
+    //print_r($_POST);
+    $curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://mgny.force.com/sales/services/apexrest/lead/add',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>json_encode($_POST),
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: application/json',
+    'Origin: https://vista65lottery.com/',
+    //'Cookie: BrowserId=KraePd8zEeyVkP9MTE2e7Q; CookieConsentPolicy=0:1; LSKey-c$CookieConsentPolicy=0:1'
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
+}
